d9dcf488cd86441c714b79aace612c892672e4c1
Ina0l <hugo75010paris@gmail.com>
2025-04-29 13:21:58 +0200

    run every test in one

---
+++ run_tests.py
@@ -0,0 +1,20 @@
+from os import listdir
+from os.path import isfile, join
+
+from readers import code_reader
+
+
+if __name__ == "__main__":
+    paths = []
+    directories = ["scripts\\tests"]
+    while len(directories) != 0:
+        for path in listdir(directories[0]):
+            if isfile(join(directories[0], path)): paths.append(join(directories[0], path))
+            else:
+                directories.append(join(directories[0], path))
+        directories = directories[1:]
+
+    for path in paths:
+        with open(path, "r", encoding="utf-8") as file:
+            code = file.read().split("\n")
+        code_reader.code_reader(code, 0)
